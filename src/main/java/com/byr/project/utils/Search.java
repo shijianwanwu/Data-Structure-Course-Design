c41b230d16a5e6741a284b4a9f5a7aab6ee5d9a3
lrp129 <2966631798@qq.com>
2024-06-04 17:32:48 +0800

    字符串搜索改用BM算法

--- src/main/java/com/byr/project/utils/Search.java
+++ src/main/java/com/byr/project/utils/Search.java
@@ -2,40 +2,31 @@ package com.byr.project.utils;
 
 public class Search {
     /**
-     * 判断des是否在origin中出现，若出现则为true否则为false,不能用直接用find这种要用一些算法比如kmp之类的
+     * 判断des是否在origin中出现，若出现则为true否则为false,使用BM算法
      * @param des
      * @param origin
      * @return
      */
-    public static boolean search(String des, String origin) {//使用kmp算法，查找匹配字符串
-        Search search = new Search();
-        int[] next = search.buildNext(des);
-        int i = 0, j = 0;
-        while (i < origin.length() && j < des.length()) {
-            if (j == -1 || origin.charAt(i) == des.charAt(j)) {
-                i++;
-                j++;
-            } else {
-                j = next[j];
-            }
-        }
-        return j == des.length();
-    }
+    public static boolean search(String des, String origin) {
+        int[] right = new int[256];
+        for (int c = 0; c < 256; c++)
+            right[c] = -1;
+        for (int j = 0; j < des.length(); j++)
+            right[des.charAt(j)] = j;
 
-    private int[] buildNext(String pattern) {
-        int[] next = new int[pattern.length()];
-        next[0] = -1;
-        int k = -1;
-        int j = 0;
-        while (j < pattern.length() - 1) {
-            if (k == -1 || pattern.charAt(j) == pattern.charAt(k)) {
-                ++k;
-                ++j;
-                next[j] = k;
-            } else {
-                k = next[k];
+        int M = des.length();
+        int N = origin.length();
+        int skip;
+        for (int i = 0; i <= N - M; i += skip) {
+            skip = 0;
+            for (int j = M - 1; j >= 0; j--) {
+                if (des.charAt(j) != origin.charAt(i + j)) {
+                    skip = Math.max(1, j - right[origin.charAt(i + j)]);
+                    break;
+                }
             }
+            if (skip == 0) return true;    // 找到
         }
-        return next;
+        return false;                       // 没找到
     }
 }
